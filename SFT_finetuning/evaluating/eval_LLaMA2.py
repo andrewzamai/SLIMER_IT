acb8594ba1b581f833a8a2b546b607a29b72289d
Andrew <andrewzamai@hotmail.com>
2024-06-21 16:35:13 +0200

    fixed some path_names

--- SFT_finetuning/evaluating/eval_LLaMA2.py
+++ SFT_finetuning/evaluating/eval_LLaMA2.py
@@ -87,7 +87,9 @@ if __name__ == '__main__':
     partial_evaluate = False
     print(f"\npartial_evaluate: {partial_evaluate}")
 
-    model_path_or_name = f"./merged_models/llama2_7B_{args.number_pos_samples_per_NE}pos_{args.number_neg_samples_per_NE}neg_perNE_top{args.number_NEs}NEs_{args.with_guidelines}Def-IT"
+    base_model = "meta-llama/Llama-2-7b-chat-hf"
+
+    model_path_or_name = f"./merged_models/{base_model.split('/')[-1]}_{args.number_pos_samples_per_NE}pos_{args.number_neg_samples_per_NE}neg_perNE_top{args.number_NEs}NEs_{args.with_guidelines}Def-IT"
     print(f"LLM model: {model_path_or_name}")
 
     max_new_tokens = 128
