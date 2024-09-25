6967d5af46fc15266377a8549722e3213444050e
Andrew <andrewzamai@hotmail.com>
2024-09-25 16:49:04 +0200

    enabling input prefix caching

--- src/SFT_finetuning/evaluating/evaluate_w_input_caching.py
+++ src/SFT_finetuning/evaluating/evaluate_w_input_caching.py
@@ -135,6 +135,10 @@ if __name__ == "__main__":
             print(dataset_SLIMER_format[0])
             sys.stdout.flush()
 
+            all_gold_answers = []
+            all_pred_answers = []
+            indices_per_tagName = defaultdict(list)
+
             for sample in dataset_SLIMER_format:
                 input = sample['input']
 
@@ -151,21 +155,183 @@ if __name__ == "__main__":
                     for x in sample['entities'].values()
                 ]
 
+                # this_sample_gold_answers = [x['output'] for x in sample['entities'].values()]
+
+                for tagName, values in sample['entities'].items():
+                    indices_per_tagName[tagName].append(len(all_gold_answers))
+                    all_gold_answers.append(values['output'])
+
                 prompts = [
                     prompter.generate_prompt(instruction, input)
                     for instruction, input in batch_instruction_input_pairs
                 ]
 
-                prefix_end = prompts[0].find("Istruzione: ")
-                prefix = prompts[0][0:prefix_end]
-                print(prefix)
+                #prefix_end = prompts[0].find("Istruzione: ")
+                #prefix = prompts[0][0:prefix_end+len("Istruzione: ")]
+                # print(prefix)
 
-                start_time = time.time()
+                #start_time = time.time()
                 responses = vllm_model.generate(prompts, sampling_params)
-                end_time = time.time()
-                print(f"Generation time: {end_time - start_time} seconds.")
+                #end_time = time.time()
+                #print(f"Generation time: {end_time - start_time} seconds.")
 
                 # 7) retrieve pred answers, aggregate them from chunks back to document level
-                all_pred_answers = [output.outputs[0].text.strip() for output in responses]
+                all_pred_answers.extend([output.outputs[0].text.strip() for output in responses])
+                # print(all_pred_answers)
 
-                break
+
+            # 8) Compute micro, perTagName, macro and weighted metrics
+
+            print("\ngold_answers")
+            print(all_gold_answers[0:10])
+            print("pred_answers")
+            print(all_pred_answers[0:10])
+
+            if partial_evaluate:
+                eval_result = (
+                    uniNER_official_eval_script.NEREvaluator().partial_evaluate(
+                        all_pred_answers, all_gold_answers
+                    )
+                )
+            else:
+                eval_result = uniNER_official_eval_script.NEREvaluator().evaluate(
+                    all_pred_answers, all_gold_answers
+                )
+
+            precision = round(eval_result["precision"] * 100, 2)
+            recall = round(eval_result["recall"] * 100, 2)
+            f1 = round(eval_result["f1"] * 100, 2)
+            print(
+                "\n{} ==> micro-Precision: {:.2f}, micro-Recall: {:.2f}, micro-F1: {:.2f}".format(
+                    subdataset_name, precision, recall, f1
+                )
+            )
+
+            print("\nMetrics per NE category (100%):\n")
+            this_dataset_metrics = {}
+            for tagName, indices_for_this_tagName in indices_per_tagName.items():
+                this_tagName_golds = [
+                    gold_ans
+                    for idx, gold_ans in enumerate(all_gold_answers)
+                    if idx in indices_for_this_tagName
+                ]
+                this_tagName_preds = [
+                    pred_ans
+                    for idx, pred_ans in enumerate(all_pred_answers)
+                    if idx in indices_for_this_tagName
+                ]
+                if partial_evaluate:
+                    eval_result = (
+                        uniNER_official_eval_script.NEREvaluator().partial_evaluate(
+                            this_tagName_preds, this_tagName_golds
+                        )
+                    )
+                else:
+                    eval_result = uniNER_official_eval_script.NEREvaluator().evaluate(
+                        this_tagName_preds, this_tagName_golds
+                    )
+                # eval json dumps to list before counting support
+
+                print("{} --> support: {}".format(tagName, eval_result["support"]))
+                print(
+                    "{} --> TP: {}, FN: {}, FP: {}, TN: {}".format(
+                        tagName,
+                        eval_result["TP"],
+                        eval_result["FN"],
+                        eval_result["FP"],
+                        -1,
+                    )
+                )
+                precision = round(eval_result["precision"] * 100, 2)
+                recall = round(eval_result["recall"] * 100, 2)
+                f1 = round(eval_result["f1"] * 100, 2)
+                print(
+                    "{} --> Precision: {:.2f}, Recall: {:.2f}, F1: {:.2f}".format(
+                        tagName, precision, recall, f1
+                    )
+                )
+                print("---------------------------------------------------------- ")
+                this_dataset_metrics[tagName] = {
+                    "support": eval_result["support"],
+                    "precision": precision,
+                    "recall": recall,
+                    "f1": f1,
+                }
+
+            # computing MACRO scores
+            this_dataset_precisions = [
+                this_dataset_metrics[tagName]["precision"]
+                for tagName in this_dataset_metrics
+            ]
+            this_dataset_recalls = [
+                this_dataset_metrics[tagName]["recall"]
+                for tagName in this_dataset_metrics
+            ]
+            this_dataset_f1s = [
+                this_dataset_metrics[tagName]["f1"] for tagName in this_dataset_metrics
+            ]
+            this_dataset_supports = [
+                this_dataset_metrics[tagName]["support"]
+                for tagName in this_dataset_metrics
+            ]
+            print(
+                "\n{} ==> MACRO-Precision: {:.2f} +- {:.2f}, MACRO-Recall: {:.2f} +- {:.2f}, MACRO-F1: {:.2f} +- {:.2f}".format(
+                    subdataset_name,
+                    np.average(this_dataset_precisions),
+                    np.std(this_dataset_precisions),
+                    np.average(this_dataset_recalls),
+                    np.std(this_dataset_recalls),
+                    np.average(this_dataset_f1s),
+                    np.std(this_dataset_f1s),
+                )
+            )
+
+            # computing WEIGHTED scores
+            this_dataset_supports_sum = sum(this_dataset_supports)
+            this_dataset_precisions_weighted = [
+                this_dataset_metrics[tagName]["precision"]
+                * (this_dataset_metrics[tagName]["support"] / this_dataset_supports_sum)
+                for tagName in this_dataset_metrics
+            ]
+            this_dataset_recalls_weighted = [
+                this_dataset_metrics[tagName]["recall"]
+                * (this_dataset_metrics[tagName]["support"] / this_dataset_supports_sum)
+                for tagName in this_dataset_metrics
+            ]
+            this_dataset_f1s_weighted = [
+                this_dataset_metrics[tagName]["f1"]
+                * (this_dataset_metrics[tagName]["support"] / this_dataset_supports_sum)
+                for tagName in this_dataset_metrics
+            ]
+            print(
+                "\n{} ==> Weighted-Precision: {:.2f}, Weighted-Recall: {:.2f}, Weighted-F1: {:.2f}".format(
+                    subdataset_name,
+                    np.sum(this_dataset_precisions_weighted),
+                    np.sum(this_dataset_recalls_weighted),
+                    np.sum(this_dataset_f1s_weighted),
+                )
+            )
+
+            # Finally, save predictions
+            preds_to_save = []
+            for i, sample in enumerate(dataset_SLIMER_format):
+                preds_to_save.append(
+                    {
+                        "doc_tag_pairID": sample["doc_tag_pairID"],
+                        "tagName": sample["tagName"],
+                        "gold_answers": all_gold_answers[i],
+                        "pred_answers": all_pred_answers[i],
+                    }
+                )
+
+            path_to_save_predictions = os.path.join("./predictions", args.merged_model_name.split("/")[-1])
+            if not os.path.exists(path_to_save_predictions):
+                os.makedirs(path_to_save_predictions)
+            with open(os.path.join(path_to_save_predictions, subdataset_name + ".json"), "w",
+                      encoding="utf-8", ) as f:
+                json.dump(preds_to_save, f, ensure_ascii=False, indent=2)
+            print("\n")
+
+        print("\nDONE :)")
+        sys.stdout.flush()
+
