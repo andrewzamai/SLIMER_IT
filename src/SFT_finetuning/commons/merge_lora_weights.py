36242def942795307525f2837b65c82da76e7471
Andrew <andrewzamai@hotmail.com>
2024-11-08 17:10:11 +0100

    SLIMER-IT parallel

--- src/SFT_finetuning/commons/merge_lora_weights.py
+++ src/SFT_finetuning/commons/merge_lora_weights.py
@@ -1,100 +1,30 @@
-""" merge fine-tuned LORA adapter weights with base model """
-
-import os.path
-import sys
-import time
-import shutil
 import argparse
-from huggingface_hub import login
-from src.SFT_finetuning.commons.initialization import get_HF_access_token, init_model
+import torch
+import sys
 
-
-def merge_main(
-    base_model: str = "meta-llama/Llama-2-7b-chat-hf",
-    lora_weights: str = "./saved_models/lora_weights_model",
-    merged_model_dir: str = "./where/to/store/the/merged/weights",
-    cutoff_len: int = 2048,
-    device_map: str = "auto",
-):
-    start_time = time.time()
-    # load base model with lora weights
-    tokenizer, model_to_merge = init_model(
-        base_model,
-        lora_weights=lora_weights,
-        load_8bit=False,
-        cutoff_len=cutoff_len,
-        device_map=device_map,
-    )
-
-    # merge model, then save it
-    start_merge_time = time.time()
-    merged_model = model_to_merge.merge_and_unload()
-    print(f"\n\nMerge only took {time.time() - start_merge_time} seconds\n")
-    merge_time = time.time() - start_time
-    print(f"\n\nWeights load and merge took {merge_time} seconds\n")
-
-    tokenizer.save_pretrained(merged_model_dir)
-    merged_model.save_pretrained(merged_model_dir)
-
-    merge_and_save_time = time.time() - start_time
-    print(f"\n\nAll took {merge_and_save_time} seconds\n")
-
-    # copy also training_config file if exists
-    if os.path.exists(os.path.join(path_to_lora, 'training_configs.yml')):
-        shutil.copy(os.path.join(path_to_lora, 'training_configs.yml'), os.path.join(save_model_at, 'training_configs.yml'))
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
 
 if __name__ == "__main__":
 
-    print("Merged Began")
-    sys.stdout.flush()
-
-    #HF_ACCESS_TOKEN = get_HF_access_token('./.env')
-    #login(token=HF_ACCESS_TOKEN)
-
-    base_model = "meta-llama/Llama-2-7b-chat-hf"
-    # as it is the code requires namespace/model_name format only, no more subfolders
-
     parser = argparse.ArgumentParser(description='''Llama merger parser''')
-    # adding arguments
-    parser.add_argument('--with_guidelines', action='store_true', help='Whether to use guidelines')
-    parser.add_argument('number_NEs', type=int, help='Number of NEs')
-    parser.add_argument('number_pos_samples_per_NE', type=int, help='Number of positive samples per NE')
-    parser.add_argument('number_neg_samples_per_NE', type=int, help='Number of negative samples per NE')
-    # parsing arguments
+    parser.add_argument('base_model', type=str, help='base_model')
+    parser.add_argument('path_to_LORA', type=str, help='path_to_LORA')
+    parser.add_argument('path_to_save_to', type=str, help='path_to_save_to')
     args = parser.parse_args()
-    path_to_lora = f"./trained_models/LLaMA2_7B_{args.number_pos_samples_per_NE}pos_{args.number_neg_samples_per_NE}neg_perNE_top{args.number_NEs}NEs_{args.with_guidelines}Def"
-    save_model_at = f"./merged_models/LLaMA2_7B_{args.number_pos_samples_per_NE}pos_{args.number_neg_samples_per_NE}neg_perNE_top{args.number_NEs}NEs_{args.with_guidelines}Def"
 
-    merge_main(base_model, path_to_lora, save_model_at)
+    base_model = AutoModelForCausalLM.from_pretrained(args.base_model, device_map={"": "cpu"}, torch_dtype=torch.bfloat16)
 
-    """ PUSH TO HF HUB """
+    model = PeftModel.from_pretrained(base_model, args.path_to_LORA, torch_dtype=torch.bfloat16, device_map={"": "cpu"})
 
-    """
-    from huggingface_hub import create_repo, upload_folder
-    from SFT_finetuning.commons.initialization import get_HF_access_token
+    print("merge and unload...")
+    model = model.merge_and_unload()
 
-    new_repo_name = f"xxxx/{save_model_at.split('/')[-1]}"
+    print("saving...")
+    model.save_pretrained(args.path_to_save_to)
 
-    url_new_repo_name = create_repo(
-        repo_id=new_repo_name,
-        token=HF_ACCESS_TOKEN,
-        exist_ok=False,
-        private=True,
-        repo_type='model',
-    )
-
-    print(url_new_repo_name)
-
-    
-    uploaded_folder_results = upload_folder(
-        folder_path=save_model_at,
-        repo_id=new_repo_name,
-        repo_type='model',
-        token=HF_ACCESS_TOKEN
-    )
-
-    print(uploaded_folder_results)
-
-    print("Merged and pushed to HF hub :)\n\n")
-    """
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(args.path_to_LORA)
+    except:
+        tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+    tokenizer.save_pretrained(args.path_to_save_to)
